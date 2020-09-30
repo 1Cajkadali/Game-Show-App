2718c2fda3c1dd0a94be87e6bee20ba85d43d7b1
Dalibor Čajka <cajka.dali@gmail.com>
2020-09-30 20:29:18 +0200

    checkLetter half way through

--- js/app.js
+++ js/app.js
@@ -5,6 +5,27 @@ let missed = 0;
 
 const startGame = document.getElementsByClassName('btn__reset')[0];
 
+// This hides an overlay //
+
 startGame.addEventListener('click', function() {
     document.getElementById('overlay').style.display ="none";
 });
+
+
+// Array with 5 strings //
+
+let phrases = ['Hello world', 'How you doing', 'Maybe not', 'Do not do this', 'I like you'];
+
+// This function gets us random Phrase //
+
+function getRandomPhraseAsArray() {
+    let randomNumber = Math.floor(Math.random() * phrases.length);
+    return phrases[randomNumber]
+};
+
+function checkLetter(button) {
+    let allLi = document.getElementsByTagName('li');
+};
+
+
+
