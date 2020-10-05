b3e102d727a11613af77c4b40424cfa5a16fe07b
Dalibor Čajka <cajka.dali@gmail.com>
2020-10-05 12:53:58 +0200

    Dont know what to do next...

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-
+const startGame = document.getElementsByClassName('btn__reset')[0];
+const overlay = document.getElementById('overlay');
+const phraseUl = phrase.querySelector('ul');
+const letter = document.getElementsByClassName('letter');
+const show = document.getElementsByClassName('show');
+const scoreboard = document.getElementById('scoreboard');
+const liScoreboard = scoreboard.querySelectorAll('.tries');
 let missed = 0;
 
-const startGame = document.getElementsByClassName('btn__reset')[0];
+
 
 // This hides an overlay //
 
-startGame.addEventListener('click', function() {
-    document.getElementById('overlay').style.display ="none";
+startGame.addEventListener('click', () => {
+    overlay.style.display ='none';
 });
 
 
@@ -19,31 +25,111 @@ let phrases = [
     'How you doing', 
     'Maybe not', 
     'Do not do this', 
-    'I like you'
+    'I like this'
 ];
 
-let randomPhraseSplit = null
-
-// This function gets us random Phrase and then splits it into array of characters //
+// Get random Phrase function
 
 function getRandomPhraseAsArray(arr) {
     let randomPhrase = arr[Math.floor(Math.random() * arr.length)];
-    let randomPhraseSplit = randomPhrase.split("");
-    return randomPhraseSplit
+    return randomPhrase.toUpperCase().split("");
 };
 
-console.log(getRandomPhraseAsArray(phrases))
-// do stuff any arr that is passed in, and add to `#phrase ul
 
-function addPhraseToDisplay(arr){
-    for (let i=0; i < arr.length; i++) {
-        let randomPhraseSplit[i]
+// Add phrase to display
+
+function addPhraseToDisplay(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        const li = document.createElement('li');
+        phraseUl.appendChild(li);
+        li.textContent = arr[i];
+        if (arr[i] !== ' ') {
+            li.className = 'letter';
+        } else {
+            li.className = 'space';
+        }
+    }
+}
+
+
+// CheckLetter function
+
+function checkLetter(btnClicked) {
+    const ltrCLicked = btnClicked.textContent.toUpperCase();
+    let letterFound = false;
+    
+    for (let i = 0; i < letter.length; i++) {
+        if ( ltrClicked === letter[i].textContent) {
+            letter[i].classList.add('show');
+            letterFound = true;
+        } 
     }
 
-};
+    return letterFound ? btnClicked : null;
+}
+
+// Create an addEventListener
+
+window.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        e.target.className = 'chosen';
+        e.target.disabled = true;
+
+        const letterFound = checkLetter(e.target);
+
+        if (letterFound === null) {
+            missed += 1;
+        }
+
+        if (missed >= 1 && missed <= 5){
+            const heart = liScoreboard[liScoreboard.length-missed];
+            heart.getElementsByTagName('img')[0].src = 'images/lostHeart.png';
+        }
+    }
+    checkWin();
+});
+
+// CheckWin function
+
+function checkWin() {
+    if (show.length == letter.length) {
+        overlay.style.display = '';
+        overlay.classList.add('win');
+        document.getElementsByClassName('title')[0].textContent = 'You Won!';
+    } else if (missed >= 5) {
+        overlay.style.display = '';
+        overlay.classList.add('lose');
+        document.getElementsByClassName('title')[0].textContent = 'You Lost!';
+    }
+}
 
 
-function checkLetter(button) {
-    let allLi = document.getElementsByTagName('li');
-};
+startGame.addEventListener('click', (e) => {
+    if (e.target.textContent === 'Reset') {
+        // set missed to 0
+        missed = 0;
 
+        // reset heart states
+        for (let i = 0; i < liScoreboard.length; i++) {
+            const img = liScoreboard[i].getElementsByTagName('img')[0];
+            img.src = 'images/liveHeart.png';
+        }
+        while (phraseUl.children.length > 0) {
+            phraseUl.removeChild(phraseUl.children[0]);
+        }
+        for (let i = 0; i < letterButtons.length; i++) {
+            letterButtons[i].classList.remove('chosen');
+            letterButtons[i].disabled = false;
+        }
+        overlay.classList.remove('win', 'lose');
+        const newGamePhrase = getRandomPhraseAsArray(phrases);
+        addPhraseToDisplay(newGamePhrase);
+    }
+});
+
+
+// Execution
+
+const phraseArray = getRandomPhraseAsArray(phrases);
+
+addPhraseToDisplay(phraseArray);
